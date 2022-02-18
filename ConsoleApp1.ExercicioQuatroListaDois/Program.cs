051f3aae7feb86dcde1bd81d0bde7b8003654210
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-18 17:13:42 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioQuatroListaDois/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioQuatroListaDois
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício quatro // Lista dois");
+            int Numero;
+
+            do
+            {
+                
+
+                
+                Console.WriteLine("Informe um número");
+                Numero = Convert.ToInt32(Console.ReadLine());
+
+                if (Numero % 2 == 0) 
+                    Console.WriteLine("O número é par");
+                else
+                    Console.WriteLine("O número é ímpar");
+
+
+
+            } while(true);
+        }
+    }
+}
